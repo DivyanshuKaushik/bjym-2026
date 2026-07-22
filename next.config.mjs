8476df59b36961eaa7c7e63a602a1854c503f1af
Divyanshu Kaushik <divyanshukaushik44@gmail.com>
2026-07-23 04:53:30 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,13 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: false },
   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
+  env:{
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  }
 };
 export default nextConfig;
